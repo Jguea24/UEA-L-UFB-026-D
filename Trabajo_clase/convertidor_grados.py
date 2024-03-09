26ad4c7c2af3fc99fc7fd34037c5601ff36224eb
Jguea24 <jg.grefac@uea.edu.ec>
2024-03-09 11:01:29 -0500

    ejercio actualizado

---
+++ Trabajo_clase/convertidor_grados.py
@@ -0,0 +1,18 @@
+def convertir_Celsius(celsius):
+    # Convertir a Fahrenheit
+    fahrenheit = (celsius * 9 / 5) + 32
+    # Convertir a Kelvin
+    kelvin = celsius + 273.15
+    # Devolver una tupla con los valores convertidos
+    return (fahrenheit, kelvin)
+
+# Obtener la entrada del usuario en grados Celsius
+celsius = float(input("Ingresa la temperatura en grados Celsius: "))
+
+# Llamar a la función para convertir los grados Celsius ingresados por el usuario
+grados_fahrenheit, kelvin = convertir_Celsius(celsius)
+
+# Imprimir los resultados
+print(f"{celsius} grados Celsius equivalen a {grados_fahrenheit} grados Fahrenheit ")
+
+print(f'{grados_fahrenheit} grrados Fahrenheit  equivale a {kelvin} kelvin')
