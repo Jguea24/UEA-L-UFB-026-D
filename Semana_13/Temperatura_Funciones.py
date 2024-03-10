dcc21feef049dd65e77b6c3d1120bd630be541cf
Jguea24 <jg.grefac@uea.edu.ec>
2024-03-09 20:13:01 -0500

    ejercio actualizado

--- Semana_13/Temperatura_Funciones.py
+++ Semana_13/Temperatura_Funciones.py
@@ -1,29 +1,29 @@
 #  función temperatura_promedio_ciudad calcula el
 #  promedio de temperatura semanal para una ciudad específica
-def temperatura_promedio_ciudad(ciudad_datos):
-    promedios_semanales = []
+def tem_pro_ci(ciudad_datos):
+    prom_semanales = []
 
     # Iteramos sobre cada semana en los datos de la ciudad
     for semana in ciudad_datos:
         # Sumamos las temperaturas de cada día en la semana
-        suma_temperaturas = sum(dia["Temperatura"] for dia in semana)
+        suma_tem = sum(dia["Temperatura"] for dia in semana)
         # Calculamos el promedio de temperatura de la semana y
         # lo agregamos a la lista de promedios
-        promedio_semanal = suma_temperaturas / len(semana)
-        promedios_semanales.append(promedio_semanal)
+        promedio_semanal = suma_tem / len(semana)
+        prom_semanales.append(promedio_semanal)
 
     # Calculamos el promedio general de la ciudad
-    promedio_general = sum(promedios_semanales) / len(promedios_semanales)
-    return promedio_general
+    promedio_gen = sum(prom_semanales) / len(prom_semanales)
+    return promedio_gen
 
 
 #  Creamos una funcion calcular_temperatura_promedio
 #  toma los datos específicos de cada ciudad y calcula el promedio de temperatura
-def calcular_temperatura_promedio(datos_ciudades):
+def cal_temp_pro(datos_ciudades):
     for ciudad_datos in datos_ciudades:
         for ciudad_nombre, datos_ciudad in ciudad_datos.items():
             # calcula el promedio de temperatura
-            promedio_ciudad = temperatura_promedio_ciudad(datos_ciudad)
+            promedio_ciudad = tem_pro_ci(datos_ciudad)
             print(" \n ======================================================>")
             print(f"El promedio de temperatura en {ciudad_nombre} es: {promedio_ciudad:.2f}°Celsius")
 
@@ -154,5 +154,5 @@ datos_ciudades = [
     ]}
 ]
 
-calcular_temperatura_promedio(datos_ciudades)
+cal_temp_pro(datos_ciudades)
 
